f3971076ad80d3d77804cfd26079a61705da4320
Arif SARI <72504353+arif-sari@users.noreply.github.com>
2023-06-05 00:11:04 +0300

    Create 13_pot_buzzer_led.cpp

---
+++ 13_pot_buzzer_led.cpp
@@ -0,0 +1,23 @@
+// C++ code
+//
+
+const int ledPin =10;
+const int buzzerPin =11;
+const int potPin=A0;
+void setup()
+{
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(A0,INPUT);
+}
+
+void loop()
+{
+  int deger= analogRead(A0);
+  digitalWrite(10,HIGH);
+  digitalWrite(11,HIGH);
+delay(deger);
+  digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
+  delay(deger);
+}
