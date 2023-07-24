f54d3cbac235657b18b593ee9e383461827d8f11
Arif SARI <72504353+arif-sari@users.noreply.github.com>
2023-07-24 10:11:21 +0300

    Update and rename 29_Counter.cpp to 29_Counter_gelen veri okuma.cpp

--- 29_Counter_gelen veri okuma.cpp
+++ 29_Counter_gelen veri okuma.cpp
@@ -1,6 +1,7 @@
 const int buton = 6;
 int sayac = 0;
 int butonDurum = 0;
+char gelenVeri = 0; /* gelen verinin kaydedileceği değişken */
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,6 +10,12 @@ void setup() {
 }
 
 void loop() {
+
+  if (Serial.available() > 0) { /* bilgisayardan veri gelmesini bekliyoruz */
+   gelenVeri = Serial.read(); /* bilgisayardan gelen karakteri oku */
+   Serial.print("gelen veri: ");
+   Serial.println(gelenVeri); /* bilgisayardan gelen veriyi bilgisayara geri yolluyoruz */
+   }
   // put your main code here, to run repeatedly:
   butonDurum = digitalRead(6);
   if (butonDurum == HIGH) {
